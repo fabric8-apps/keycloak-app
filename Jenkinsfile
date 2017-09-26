59d9f6bce095abbac1ba84e120979450503e9961
rawlingsj <rawlingsj80@gmail.com>
2017-09-26 12:40:10 +0100

    initial checkin

---
+++ Jenkinsfile
@@ -0,0 +1,36 @@
+#!/usr/bin/groovy
+@Library('github.com/fabric8io/fabric8-pipeline-library@master')
+def utils = new io.fabric8.Utils()
+
+releaseNode {
+  try {
+    checkout scm
+    readTrusted 'release.groovy'
+
+    if (utils.isCI()) {
+
+      mavenCI{}
+
+    } else if (utils.isCD()) {
+      sh "git remote set-url origin git@github.com:fabric8-apps/keycloak-app.git"
+
+      def pipeline = load 'release.groovy'
+      def stagedProject
+
+      stage('Stage') {
+        stagedProject = pipeline.stage()
+      }
+
+      stage('Promote') {
+        pipeline.release(stagedProject)
+      }
+
+      stage ('Update downstream dependencies'){
+        pipeline.updateDownstreamDependencies(stagedProject)
+      }
+    }
+  } catch (err) {
+    hubot room: 'release', message: "${env.JOB_NAME} failed: ${err}"
+    error "${err}"
+  }
+}
---
+++ release.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/groovy
+def stage(){
+  return stageProject{
+    project = 'fabric8-apps/keycloak-app'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.apps'
+    githubOrganisation = 'fabric8-apps'
+    artifactIdToWatchInCentral = 'keycloak-app'
+    artifactExtensionToWatchInCentral = 'jar'
+    promoteToDockerRegistry = 'docker.io'
+    dockerOrganisation = 'fabric8'
+    imagesToPromoteToDockerHub = []
+    extraImagesToTag = null
+  }
+}
+
+def updateDownstreamDependencies(stagedProject) {
+  pushPomPropertyChangePR {
+    propertyName = 'nexus-app.version'
+    projects = [
+            'fabric8io/fabric8-tenant-jenkins'
+    ]
+    version = stagedProject[1]
+  }
+}
+return this;
